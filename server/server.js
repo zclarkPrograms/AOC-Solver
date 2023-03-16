05f0d9bd71e056c32af676be940ad2f6c554157c
zclarkPrograms <Zachary.Clark323@gmail.com>
2023-03-16 01:16:34 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+app.post('/solve', (req, res) => {
+***REMOVED***
+    // Call your Advent of Code solver function with year, day, and puzzleInput
+    const solution = solveAdventOfCode(year, day, puzzleInput);
+    console.log(req.body);
+***REMOVED***
+***REMOVED***;
+
+// Start server
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+***REMOVED***;
+
+function solveAdventOfCode(year, day, puzzleInput) {
+    return `year: ${year***REMOVED***, day: ${day***REMOVED***, puzzleInput: ${puzzleInput***REMOVED***`;
+***REMOVED***
