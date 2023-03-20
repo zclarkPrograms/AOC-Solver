2a62343e3db8c5dc84ea344e6de9fbf34188fe56
zclarkPrograms <Zachary.Clark323@gmail.com>
2023-03-20 15:01:13 -0400

    mongodb_connection_url environment variable is now used instead of mongodb_password

--- server/server.js
+++ server/server.js
@@ -70,10 +70,9 @@ app.get('/removeAll', async (req, res) => {
     res.send(await Solution.deleteMany());
 })
 
-const CONNECTION_URL = `mongodb+srv://SlothsAllTheWay:${process.env.MONGODB_PASSWORD}@cluster0.rulcp6z.mongodb.net/?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true })
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true })
     .then(() =>{
         app.listen(PORT, () =>{
             console.log(`Server running on port: ${PORT} `)
